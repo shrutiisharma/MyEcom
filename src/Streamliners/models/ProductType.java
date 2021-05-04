edd010a79648f3d5af0a6a1609424fafc1b26d3b
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-05-04 23:58:52 +0530

    Product Type Class

---
+++ src/Streamliners/models/ProductType.java
@@ -0,0 +1,8 @@
+package Streamliners.models;
+
+public class ProductType {
+
+    //creating constants
+    //used static so as to access the integer values without creating objects
+    public static final int TYPE_WB = 0, TYPE_VB = 1;
+}
