45cd6e828df24077cf5894d1fd4da572d4dea42d
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-05-04 23:52:20 +0530

    Main Class

--- src/Streamliners/Main.java
+++ src/Streamliners/Main.java
@@ -1,74 +1,74 @@
 package Streamliners;
 
+import Streamliners.models.Cart;
 import Streamliners.models.Product;
 import Streamliners.models.Variant;
-import Streamliners.models.VariantsBasedProduct;
-import Streamliners.models.WeightBasedProduct;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
-        //creating an object of the Product class
-        Product product = new Product("Mango", "mangoImg");
-        System.out.println(product);
-
 /*
-        //null
-        Product product1 = new Product();
-        System.out.println(product1);
+        //hashMap introduction
 
-        o/p = Product{name='null', imageURL='null'}
+        HashMap<String,Integer> map = new HashMap<>();
 
-        //null pointer exception error
-         System.out.println(product1.name.toUpperCase());
+        map.put("A", 454845);
+        map.put("B", 879452);
+        map.put("C", 238856);
+
+        System.out.println(map.containsKey("A"));      //true
 */
 
 
-        //weightBasedProduct
-        WeightBasedProduct weightBasedProduct = new WeightBasedProduct(
-                "Apple", "AppleImg", 100, 1);
-
-        System.out.println(weightBasedProduct);
-
-/*
-        //list data type
-        List<String> strings = new ArrayList<>(
-                Arrays.asList("A", "B", "C")
-        );
-        System.out.println(strings);
-*/
+        //create products
+        Product apple = new Product("Apple", "img", 100, 0.5f ),
+                orange = new Product("Orange", "img", 80, 0.5f ),
+                kiwi = new Product("Kiwi", "img", new ArrayList<>(
+                        Arrays.asList(
+                                new Variant("500g", 96),
+                                new Variant("1kg", 180)
+                        ))),
+                surfExcel = new Product("Surf Excel", "img", new ArrayList<>(
+                        Collections.singletonList(new Variant("1kg", 180))));
 
 
-        //variant
-        List<Variant> variants = new ArrayList<>(
-                        Arrays.asList(new Variant("500g", 90 ), new Variant("1kg", 180))
-                );
+        //create object of Cart class
+        Cart cart = new Cart();
 
+        //add items
+        cart.add(apple, 2.5f);
 
-        //variantBasedProduct
-        VariantsBasedProduct variantsBasedProduct = new VariantsBasedProduct(
-                "Kiwi", "kiwiImg", variants);
+        cart.add(orange, 2.5f);
 
-        System.out.println(variantsBasedProduct);
+        cart.add(kiwi, kiwi.variants.get(1));
+        cart.add(kiwi, kiwi.variants.get(1));
+        cart.add(kiwi, kiwi.variants.get(1));
 
+        cart.add(surfExcel, surfExcel.variants.get(0));
+        cart.add(surfExcel, surfExcel.variants.get(0));
 
-        //equals() and hashCode() methods
-        Product a = new Product("a", "");
-        Product b = new Product("a", "");
-        System.out.println(a.hashCode());
-        System.out.println(b.hashCode());
-        System.out.println(a.equals(b));
+        System.out.println(cart);
 
+        //remove items
+        cart.remove(orange);
+        System.out.println();
+        System.out.println(cart);
 
-        //Adding products in list
-        List<Product> products = new ArrayList<>(
-                Arrays.asList(weightBasedProduct, variantsBasedProduct)
-        );
+        cart.decrement(surfExcel, surfExcel.variants.get(0));
+        System.out.println();
+        System.out.println(cart);
 
-        System.out.println(products);
+        cart.decrement(surfExcel, surfExcel.variants.get(0));
+        System.out.println();
+        System.out.println(cart);
+
+        cart.remove(kiwi);
+        System.out.println();
+        System.out.println(cart);
+
     }
 }
